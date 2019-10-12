0f17b28787bbdaa3fbf710cf45d10ff29af99582
deploy.sh <blakertarter@gmail.com>
2019-10-11 21:49:53 -0500

    File cleanup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from '@emotion/styled'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import AboutMe from './components/AboutMe';
-import MyWork from './components/MyWork';
-import FindMe from './components/FindMe';
-import Footer from './components/Footer';
-import { fontFamily } from './globals/fonts';
-import './vendor/normalize.css';
-import db from './db.json';
-import { darkModeQuery } from './globals/sizes';
-import { darkBackground, white, black } from './globals/colors';
+import Navbar from '../Navbar';
+import Hero from '../Hero';
+import AboutMe from '../AboutMe';
+import MyWork from '../MyWork';
+import FindMe from '../FindMe';
+import Footer from '../Footer';
+import { fontFamily } from '../../globals/fonts';
+import '../../vendor/normalize.css';
+import db from '../../db.json';
+import { darkModeQuery } from '../../globals/sizes';
+import { darkBackground, white, black } from '../../globals/colors';
 
 const Container = styled('div')`
   height: 100%;
